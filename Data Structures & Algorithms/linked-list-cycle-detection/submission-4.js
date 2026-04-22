164bd8243334ce24fd655d906f38e85ab31671d6
Mike Gutierrez <11773573+mikegutierrez@users.noreply.github.com>
2026-04-21 17:26:01 -0700

    Add: linked-list-cycle-detection - submission-4

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-4.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        const set = new Set();
+        let current = head;
+        while (current) {
+            if (current.next === null) return false;
+            if (set.has(current)) return true;
+            set.add(current);
+            current = current.next;
+        }
+        return false;
+    }
+}
