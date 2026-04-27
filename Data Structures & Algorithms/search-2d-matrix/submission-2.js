cb735886361f32c4216d4a751ec85a47d9e63d3b
Mike Gutierrez <11773573+mikegutierrez@users.noreply.github.com>
2026-04-27 15:54:37 -0700

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.js
@@ -0,0 +1,53 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    // O(m log n)
+    searchMatrixFirstPass(matrix, target) {
+        for (const row of matrix) {
+            const leftVal = row[0];
+            const rightVal = row[row.length - 1];
+            if (leftVal <= target && rightVal >= target) {
+                let left = 0;
+                let right = row.length - 1;
+                while (left <= right) {
+                    const middleIdx = Math.floor((left + right) / 2);
+                    const middleVal = row[middleIdx];
+                    if (middleVal === target) return true;
+                    if (target < middleVal) right = middleIdx - 1;
+                    if (target > middleVal) left = middleIdx + 1;
+                }
+            }
+        }
+        return false;
+    }
+
+    // O(log(m * n))
+    searchMatrix(matrix, target) {
+        const m = matrix.length;
+        const n = matrix[0].length;
+        let left = 0;
+        let right = m * n - 1;
+
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+
+            // dividing the flat index by the row length tells 
+            // how many complete rows fit before this index
+            const row = Math.floor(mid / n);
+
+            // the remainder after dividing by row length tells 
+            // how far into the current row you are
+            const col = mid % n;
+
+            const midVal = matrix[row][col];
+
+            if (midVal === target) return true;
+            if (target < midVal) right = mid - 1;
+            if (target > midVal) left = mid + 1;
+        }
+        return false;
+    }
+}
